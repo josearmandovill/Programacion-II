15d6497d265d71efd263274448ea06d229d4d5c7
josearmandovill <djrunay@gmail.com>
2025-08-27 00:09:08 -0400

    Update ejercicio1.py

--- Practica-1/ejercicio1.py
+++ Practica-1/ejercicio1.py
@@ -1,1 +1,29 @@
+class ecuacionlineal:
+    def _init_(self, a, b, c, d, e, f):
+        self.__a = a
+        self.__b = b
+        self.__c = c
+        self.__d = d
+        self.__e = e
+        self.__f = f
 
+    def solucion(self):
+        return (self._a * self.d - self.b * self._c) != 0
+
+    def X(self):
+        return (self._e * self.d - self.b * self.f) / (self.a * self.d - self.b * self._c)
+
+    def Y(self):
+        return (self._a * self.f - self.e * self.c) / (self.a * self.d - self.b * self._c)
+
+valores = input("ingrese a, b, c, d, e, f separados: ").split()
+a, b, c, d, e, f = map(float, valores)
+
+eq = ecuacionlineal(a, b, c, d, e, f)
+
+if eq.solucion():
+    print("La solución del sistema es:")
+    print("x =", eq.X())
+    print("y =", eq.Y())
+else:
+    print("La ecuación no tiene solución")
