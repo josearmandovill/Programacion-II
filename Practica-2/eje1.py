ffff3faa0be025ef86eead6ade22c79064e3fc33
josearmandovill <djrunay@gmail.com>
2025-09-10 10:58:00 -0400

    Update eje1.py

--- Practica-2/eje1.py
+++ Practica-2/eje1.py
@@ -1,1 +1,49 @@
 
+import math
+
+class Vector:
+    def _init_(self, x: float = 0.0, y: float = 0.0):
+        self.x = float(x)
+        self.y = float(y)
+        
+    def _matmul_(self, other: "Vector") -> float:
+        return self.x * other.x + self.y * other.y
+
+    def _xor_(self, other: "Vector") -> float:
+        return self.x * other.y - self.y * other.x
+
+    def length(self) -> float:
+        return math.sqrt(self.x*2 + self.y*2)
+
+    def is_perpendicular(self, other: "Vector") -> bool:
+        return (self @ other) == 0
+
+    def is_parallel(self, other: "Vector") -> bool:
+        return (self ^ other) == 0
+
+    def projection(self, other: "Vector") -> "Vector":
+        factor = (self @ other) / (other.length()**2)
+        return Vector(factor * other.x, factor * other.y)
+
+    def component(self, other: "Vector") -> float:
+        return (self @ other) / other.length()
+
+    def _repr_(self) -> str:
+        return f"Vector({self.x:.2f}, {self.y:.2f})"
+  
+
+
+a = Vector(3, 4)
+b = Vector(-4, 3)
+
+print("a =", a)
+print("b =", b)
+
+print("a · b =", a @ b)
+print("a × b =", a ^ b)
+
+print("¿Perpendiculares?", a.is_perpendicular(b))
+print("¿Paralelos?", a.is_parallel(b))
+
+print("Proyección de a sobre b:", a.projection(b))
+print("Componente de a sobre b:", a.component(b))
